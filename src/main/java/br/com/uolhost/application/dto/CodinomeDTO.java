c81b7031e7d2e4d34dbc30627ab825b006b0fde0
Tiago <ti.almeida58@gmail.com>
2024-03-11 16:46:17 -0300

    feat: creation of DTOS for league group

---
+++ src/main/java/br/com/uolhost/application/dto/CodinomeDTO.java
@@ -0,0 +1,24 @@
+package br.com.uolhost.application.dto;
+
+import javax.xml.bind.annotation.XmlValue;
+
+public class CodinomeDTO {
+
+    @XmlValue
+    private String codinome;
+
+    public CodinomeDTO() {
+    }
+
+    public CodinomeDTO(String codinome) {
+        this.codinome = codinome;
+    }
+
+    public String getCodinome() {
+        return codinome;
+    }
+
+    public void setCodinome(String codinome) {
+        this.codinome = codinome;
+    }
+}
